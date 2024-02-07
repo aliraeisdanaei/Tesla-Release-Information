644e51e5426e130a71755fef18a02d3ff1198306
aliraeisdanaei <mrrookie2@gmail.com>
2024-02-07 18:20:00 -0500

    add script to collect descriptions of updates

---
+++ Data/get_update_desc.py
@@ -0,0 +1,36 @@
+import json
+import sys
+
+RELEASE_FAMILIES_FILENAME = 'release_families.json'
+
+with open(RELEASE_FAMILIES_FILENAME) as f:
+    release_family_data = json.load(f)
+
+update_name = sys.argv[1]
+print('Getting all unique descriptions for: ', update_name)
+
+update_descriptions = ''
+
+unique_update_desc = set()
+
+for release_family in release_family_data:
+    for update in release_family['updates']:
+        if update_name == update['update_name']:
+            update_desc = update['update_desc']
+
+            if update_desc not in unique_update_desc:
+                unique_update_desc.add(update_desc)
+
+                update_descriptions += update_desc
+                update_descriptions += '\n'
+                update_descriptions += '\n'
+
+if len(unique_update_desc) == 0:
+    print('Nothing found :(')
+else:
+    print('Found ', len(unique_update_desc), ' unique update descriptions. :)')
+    output_filename = f'Descriptions/{update_name}.txt'
+    print('Saving descriptions to: ', output_filename)
+
+    with open(output_filename, 'w') as f:
+        f.write(update_descriptions)
